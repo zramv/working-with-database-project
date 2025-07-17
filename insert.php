4c6cfe8a4d1a9c093ba8aee13132a8fb975b8c7b
Dhaifallah <defsxd@gmail.com>
2025-07-17 11:06:14 +0300

    improve some output

--- insert.php
+++ insert.php
@@ -7,16 +7,16 @@ $name = $data['name'];
 $age = $data['age'];
 
 if(!isset($name) or !isset($age) or empty($name) or empty($age)){
-  echo json_encode("please,Insert name and age.",JSON_UNESCAPED_UNICODE);
+  echo json_encode("Please, Insert name and age.",JSON_UNESCAPED_UNICODE);
   die();
 } 
 if(strlen($name) > 25 or strlen($age) > 3){
-  echo json_encode("maximum length of name is 25 and 3 for age.",
+  echo json_encode("Maximum length of name is '25' and '3' for age.",
   JSON_UNESCAPED_UNICODE);
   die();
 }
 if(!is_int($age)){
-  echo json_encode("the age must be a number",
+  echo json_encode("The age must be a number",
   JSON_UNESCAPED_UNICODE);
   die();
 }
@@ -26,4 +26,4 @@ $stmt->bindValue('name',$name);
 $stmt->bindValue('age',$age);
 $stmt->execute();
 
-echo json_encode("insert is successes",JSON_UNESCAPED_UNICODE);
+echo json_encode("Insert is successes",JSON_UNESCAPED_UNICODE);
